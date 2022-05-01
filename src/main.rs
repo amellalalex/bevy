efbd1b300166ed1888500f72d1d237b52a33b778
amellalalex <github@alexamellal.com>
2022-05-01 12:52:51 -0300

    added something

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(HandyPlugin)
+        .run();
+}
+
+fn main_old() {
+    App::new()
+        .add_plugins(DefaultPlugins)
         .add_startup_system(add_people)
         .add_system(hello_world)
         .add_system(greet_people)
@@ -13,20 +20,34 @@ fn hello_world() {
     println!("hello world!");
 }
 
+// Create some components
 #[derive(Component)]
 struct Person;
 
 #[derive(Component)]
 struct Name(String);
 
+// Spawn those components
 fn add_people(mut commands: Commands) {
     commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
     commands.spawn().insert(Person).insert(Name("Joe Mama".to_string()));
     commands.spawn().insert(Person).insert(Name("Big Johnny".to_string()));
 }
 
+// Manipulate those components
 fn greet_people(query: Query<&Name, With<Person>>) {
     for name in query.iter() {
         println!("Goodday {}!", name.0);
+    }
+}
+
+// Create a plugin
+pub struct HandyPlugin;
+
+impl Plugin for HandyPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(add_people)
+            .add_system(hello_world)
+            .add_system(greet_people);
     }
 }
